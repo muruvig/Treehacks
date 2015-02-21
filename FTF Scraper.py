995bba3fe12cabc1335f5bb5ec9d9221feb27510
Vignesh Muruganantham <muruvig@gmail.com>
2015-02-21 12:25:51 -0800

    scraper split update

--- FTF Scraper.py
+++ FTF Scraper.py
@@ -2,7 +2,7 @@ import urllib.request
 
 def connect(url):
 	website = urllib.request.urlopen(url)
-	input = website.read()
+	input = website.read().split()
 	print(input)
 
 connect("http://www.cbioportal.org/webservice.do?cmd=getClinicalData&case_set_id=ov_tcga_all")
